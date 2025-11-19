4fc4cce021b1a1ccac8756567f36772863e5cfba
obulka <oleg.bulka@gmail.com>
2025-11-19 12:22:01 +0200

    TAT-104: comment out eks, since he is not working and fail other modules

--- modules/variables.tf
+++ modules/variables.tf
@@ -89,85 +89,85 @@ variable "role_arn" {
   default = null
 }
 
-variable "cluster_availability_zone_1" {
-  description = "One of availability zones where our nodes will be created"
-  type        = string
-}
-
-variable "cluster_availability_zone_2" {
-  description = "One of availability zones where our nodes will be created"
-  type        = string
-}
-
-
-variable "private_subnet_cidr_block_1" {
-  description = "Subnet for our nodes"
-  type        = string
-}
-
-variable "private_subnet_cidr_block_2" {
-  description = "Subnet for our nodes"
-  type        = string
-}
-
-variable "public_subnet_cidr_block_1" {
-  description = "Subnet for nat to let for our node connect to the internet"
-  type        = string
-}
-
-
-variable "public_subnet_cidr_block_2" {
-  description = "Subnet for ELB which will be later connected to ingress controller"
-  type        = string
-}
-
-
-variable "min_size" {
-  description = "Minimum number of nodes to have in the EKS cluster"
-  type        = number
-}
-variable "max_size" {
-  description = "Maximum number of nodes to have in the EKS cluster"
-  type        = number
-}
-
-variable "desired_size" {
-  description = "Desired number of nodes to have in the EKS cluster"
-  type        = number
-}
-
-
-variable "eks_cluster_name" {
-  description = "The name to use for the EKS cluster"
-  type        = string
-}
-
-variable "environment_name" {
-  description = "The name of environment where the EKS cluster will be created"
-  type        = string
-}
-
-variable "eks_cluster_k8s_version" {
-  description = "The kubernetes version that will be used for our cluster"
-  type        = string
-}
-
-
-variable "node_instance_types" {
-  description = "The types of EC2 instances to run in the node group"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "The id of vpc where our application will be deployed"
-  type        = string
-}
-
-variable "public_route_table_id" {
-  description = "The id of the public route table where our nat and elb will be deployed"
-  type        = string
-}
-
+# # variable "cluster_availability_zone_1" {
+#   description = "One of availability zones where our nodes will be created"
+#   type        = string
+# }
+#
+# variable "cluster_availability_zone_2" {
+#   description = "One of availability zones where our nodes will be created"
+#   type        = string
+# }
+#
+#
+# variable "private_subnet_cidr_block_1" {
+#   description = "Subnet for our nodes"
+#   type        = string
+# }
+#
+# variable "private_subnet_cidr_block_2" {
+#   description = "Subnet for our nodes"
+#   type        = string
+# }
+#
+# variable "public_subnet_cidr_block_1" {
+#   description = "Subnet for nat to let for our node connect to the internet"
+#   type        = string
+# }
+#
+#
+# variable "public_subnet_cidr_block_2" {
+#   description = "Subnet for ELB which will be later connected to ingress controller"
+#   type        = string
+# }
+#
+#
+# variable "min_size" {
+#   description = "Minimum number of nodes to have in the EKS cluster"
+#   type        = number
+# }
+# variable "max_size" {
+#   description = "Maximum number of nodes to have in the EKS cluster"
+#   type        = number
+# }
+#
+# variable "desired_size" {
+#   description = "Desired number of nodes to have in the EKS cluster"
+#   type        = number
+# }
+#
+#
+# variable "eks_cluster_name" {
+#   description = "The name to use for the EKS cluster"
+#   type        = string
+# }
+#
+# variable "environment_name" {
+#   description = "The name of environment where the EKS cluster will be created"
+#   type        = string
+# }
+#
+# variable "eks_cluster_k8s_version" {
+#   description = "The kubernetes version that will be used for our cluster"
+#   type        = string
+# }
+#
+#
+# variable "node_instance_types" {
+#   description = "The types of EC2 instances to run in the node group"
+#   type        = list(string)
+# }
+#
+# variable "vpc_id" {
+#   description = "The id of vpc where our application will be deployed"
+#   type        = string
+# }
+#
+# variable "public_route_table_id" {
+#   description = "The id of the public route table where our nat and elb will be deployed"
+#   type        = string
+# }
+#
 variable "env" {
   type        = string
   description = "Environment to deploy resources"
