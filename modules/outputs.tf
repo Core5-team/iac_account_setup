5e2c451265cd366f1a55c2f1bf0a06ea20bed695
oalexyeyev <alexyeyev@gmail.com>
2025-12-02 00:41:09 +0200

    CORE5-13: Refactor output values in outputs.tf to conditionally use Jenkins variables

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = var.enable_jenkins ? module.vpc[0].vpc_id : var.vpc_id
 }
 
 output "igw_id" {
-  value = module.vpc.internet_gateway_id
+  value = var.enable_jenkins ? module.vpc[0].internet_gateway_id : var.igw_id
 }
 
 output "key_pair_name" {
-  value = aws_key_pair.jenkins-key-pair.key_name
+  value = var.enable_jenkins ? aws_key_pair.jenkins-key-pair[0].key_name : var.key_pair
 }
